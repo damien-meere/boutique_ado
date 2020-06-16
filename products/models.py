f9c92b3e97cc9c062ec556f5827114a34d1be451
Damien Meere <damien.meere@bt.com>
2020-06-16 11:11:59 +0000

    product app created, images added

---
+++ products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friently_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    Category
