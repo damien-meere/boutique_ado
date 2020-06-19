06cfff48af41000a82e21c8b71caf560a3e1803b
Damien Meere <damien.meere@bt.com>
2020-06-19 14:01:57 +0000

    added 'add to bag' functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,26 @@ from django.shortcuts import render
 def view_bag(request):
     """ View to return shopping bag page"""
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+    quantity = request.POST.get('quantity')
+    redirect_url = request.POST.get('redirect_url')
+    # the contents of the shopping 'bag' will persist within the HTTP session
+    # maintained between the cliet browser and the django server itself
+    bag = request.session.get('bag', {})
+
+    # Within the bag dictionary, create a key of the items id and
+    # set it equal to the quantity. If the item is already in the bag in
+    # other words if there's already a key in the bag dictionary matching
+    # this product id, increment its quantity accordingly.
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    # overwrite the bag variable in the session with the updated version
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents', # this addition means that anytime we need to access the bag contents in any template across the entire site they'll be available to us
             ],
         },
     },
@@ -152,3 +153,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
