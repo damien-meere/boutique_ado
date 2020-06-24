7555eefdae0a1dc98a84b8caec3760cf1c5abf41
Damien Meere <damien.meere@bt.com>
2020-06-24 12:46:14 +0000

    Product sizing logic in place

--- bag/contexts.py
+++ bag/contexts.py
@@ -16,15 +16,34 @@ def bag_contents(request):
     # to an empty dictionary if not.
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    # The item data will just be the quantity. But in the case of an
+    # item that has sizes the sitem data will be a dictionary of all
+    # the items by size.
+    for item_id, item_data in bag.items():
+        # only want to execute this code if the item has no sizes.
+        # Which will be evident by checking whether or not the item
+        # data is an integer.
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        # if the item has sizes
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -12,19 +12,36 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     # the contents of the shopping 'bag' will persist within the HTTP session
     # maintained between the cliet browser and the django server itself
     bag = request.session.get('bag', {})
 
-    # Within the bag dictionary, create a key of the items id and
-    # set it equal to the quantity. If the item is already in the bag in
-    # other words if there's already a key in the bag dictionary matching
-    # this product id, increment its quantity accordingly.
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    # if the products has a size parameter
+    if size:
+        # If the item is already in the bag.
+        if item_id in list(bag.keys()):
+            # check if another item of the same id and same size exists
+            if size in bag[item_id]['items_by_size'].keys():
+                # increment the quantity for that size
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                # otherwise just set it equal to the quantity.
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    # if the products has no size parameter
     else:
-        bag[item_id] = quantity
+        # Within the bag dictionary, create a key of the items id and
+        # set it equal to the quantity. If the item is already in the bag in
+        # other words if there's already a key in the bag dictionary matching
+        # this product id, increment its quantity accordingly.
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     # overwrite the bag variable in the session with the updated version
     request.session['bag'] = bag
