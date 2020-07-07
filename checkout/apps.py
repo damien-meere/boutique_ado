1424baff8fc42075ce4123d0dc1aaca7bbc0aeb0
Damien Meere <damien.meere@bt.com>
2020-07-07 15:22:34 +0000

    added forms, admin and signals

--- checkout/apps.py
+++ checkout/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class CheckoutConfig(AppConfig):
     name = 'checkout'
+
+    # verriding the ready method and importing our signals module.
+    # With that done, every time a line item is saved or deleted.
+    # Our custom update total model method will be called. Updating
+    # the order totals automatically.
+    def ready(self):
+        import checkout.signals
