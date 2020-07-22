dfe09bd7bb339c8a34aa7677a54e04453e00516c
Damien Meere <damien.meere@bt.com>
2020-07-22 11:40:52 +0000

    Basic Checkout Functionality Complete

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -8,9 +8,9 @@
 
 // slice off the first and last character on each since they'll have 
 // quotation marks which we don't want.
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -44,4 +44,41 @@ card.addEventListener('change', function(event){
     }else {
         errorDiv.textContent = '';
     }
+});
+
+// Handle Form Submission
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    // Prevent defualt POST action
+    ev.preventDefault();
+    // Instesd execute the following code
+    // Disable both the card element and the submit button to prevent multiple submissions.
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    // Provide the card to stripe
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    // then execute this function on the result.
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            // After providing error notification, reenable card and submit button
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        // If status of the payment intent comes back as successful, Submit Form
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
